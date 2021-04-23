196f554d1fc0c3510b4c771c070cdb653b3464a1
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2021-04-23 13:53:26 +0100

    basic structure ok task-management

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 import { LoginComponent } from './login/login.component';
 import { SigninComponent } from './signin/signin.component';
+import { TaskManagementComponent } from './task-management/task-management/task-management.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signin', component: SigninComponent },
+  { path: 'task-management', component: TaskManagementComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   // { path: '**', component: PageNotFoundComponent },
 ]; // sets up routes constant where you define your routes
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,14 @@ import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { SigninComponent } from './signin/signin.component';
 import { AppRoutingModule } from './app-routing.module';
+import { TaskManagementComponent } from './task-management/task-management/task-management.component';
+import { CreateTaskComponent } from './task-management/create-task/create-task.component';
+import { SearchTaskComponent } from './task-management/search-task/search-task.component';
+import { TaskComponent } from './task-management/task/task.component';
+import { TasksContainerComponent } from './task-management/tasks-container/tasks-container.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, SigninComponent],
+  declarations: [AppComponent, LoginComponent, SigninComponent, TaskManagementComponent, CreateTaskComponent, SearchTaskComponent, TaskComponent, TasksContainerComponent],
   imports: [
     AppRoutingModule,
     BrowserModule,
